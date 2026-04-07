6eadead779d0d7f76a35f97f1a09c69c911def53
Emanuel Alessi <emanuelalessi14@gmail.com>
2026-04-07 11:46:12 -0300

    changes

--- gtm-presupuestos/next.config.ts
+++ gtm-presupuestos/next.config.ts
@@ -3,9 +3,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
-  experimental: {
-    turbo: undefined,
-  },
 };
 
 export default nextConfig;

0dbc18fa7380c8945661c0e5db841c2cc76e333f
Emanuel Alessi <emanuelalessi14@gmail.com>
2026-04-23 12:17:05 -0300

    Update next.config.ts

--- gtm-presupuestos/next.config.ts
+++ gtm-presupuestos/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/presupuestos",
   bundlePagesRouterDependencies: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
